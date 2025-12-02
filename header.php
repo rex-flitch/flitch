8b38c050fb9a4425b3ac79dac8119d1ebfe0deb4
Rex Frisbey <rex@flitchcreative.com>
2025-12-02 16:00:21 -0700

    Changing header to test

--- header.php
+++ header.php
@@ -93,10 +93,10 @@
                     </nav>
                 </div>
                 <div class="col-2 contact-area">
-                    <div class="contact-button">
+                    <div class="contact-button test">
                     <?php
                         $contact = get_field('contact_number', 'options');
-                        echo "<a href='tel:" . $contact . "' class='btn-green'>" . $contact . "</a>";
+                        echo "<a href='tel:" . $contact . "' class='btn-green'>" . $contact . " test</a>";
                     ?>
                     </div>
                     <div class="contact-icon">
